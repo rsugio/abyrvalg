3a20ee9fadb09c7a79ed60a85405355442e69068
chumpa <chumpa@yandex.ru>
2021-04-06 19:35:06 +0300

    раскидано в CommandsCPI и CommandsPI

--- src/Config.kt
+++ src/Config.kt
@@ -1,4 +1,8 @@
+package io.rsug.abyrvalg
+
 import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.cookies.*
 import io.ktor.client.features.logging.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -17,7 +21,7 @@ import java.util.*
 enum class AuthEnum { Basic }
 
 // в xmlSoap много полиморфизма внутри SOAP-Envelope
-val xmlSoap = k5.xml()
+val xmlSoap = xml()
 
 @Serializable
 data class Config(
@@ -27,6 +31,33 @@ data class Config(
 ) {
     @Transient
     lateinit var client: HttpClient
+
+    fun createClient() {
+        client = HttpClient(CIO) {
+            engine {
+                threadsCount = 2
+            }
+            install(HttpCookies) {
+                storage = AcceptAllCookiesStorage()
+            }
+            install(Logging) {
+//                logger = httpLogger
+                level = httpLogLevel
+            }
+        }
+    }
+    fun init() {
+        runBlocking {
+            tenants.forEach {
+                val ten = it.value
+                ten.nick = it.key
+                if (ten.autologin) {
+                    ten.login(client)
+                }
+                println("INFO: ${ten.nick} autologged=${ten.logged}") //TODO - logger
+            }
+        }
+    }
 
     @Serializable
     data class PI(
@@ -114,6 +145,7 @@ data class Config(
     )
 }
 
+
 fun requireJson(resp: HttpResponse) {
     require(resp.status.equals(HttpStatusCode.OK))
     require(resp.contentType()!!.match(ContentType.Application.Json))
@@ -127,125 +159,3 @@ fun requireXml(resp: HttpResponse) {
     )
 }
 
-class CheckCapabilitiesCPI(val config: Config, val nick: String) {
-    val tenant: Config.Tenant
-
-    init {
-        tenant = config.tenants[nick] ?: error("Не могу найти $nick в конфиге, шары залил что-ли уже с утра?!")
-    }
-
-    fun exec(): StringBuilder {
-        val caps = StringBuilder()
-        var ok = false
-        runBlocking {
-            tenant.loginIfNeeded(config.client)
-            var resp: HttpResponse = config.client.get("${tenant.tmn}/api/v1/\$metadata") {
-                tenant.defaultHeaders(this, ContentType.Application.Xml)
-            }
-            ok = resp.status == HttpStatusCode.OK && resp.contentType()!!.match(ContentType.Application.Xml)
-            caps.append("/api/v1 : rc=${resp.status.value} API=${if (ok) "Поддерживается" else "НеПоддерживается"}\n")
-
-            resp = config.client.get("${tenant.tmn}/itspaces/odata/1.0/workspace.svc/\$metadata") {
-                tenant.defaultHeaders(this, ContentType.Application.Xml)
-            }
-            ok = resp.status == HttpStatusCode.OK && resp.contentType()!!.match(ContentType.Application.Xml)
-            caps.append("/itspaces/odata/1.0/workspace.svc : rc=${resp.status.value} API=${if (ok) "Поддерживается" else "НеПоддерживается"}\n")
-
-            resp =
-                config.client.get("${tenant.tmn}/itspaces/Operations/com.sap.it.op.tmn.commands.dashboard.webui.CapabilityListCommand") {
-                    tenant.defaultHeaders(this)
-                }
-            ok = resp.status == HttpStatusCode.OK && resp.contentType()!!.match(ContentType.Application.Json)
-            caps.append("/itspaces/Operations/com.sap.it.op.tmn.commands.dashboard.webui.CapabilityListCommand : rc=${resp.status.value} API=${if (ok) "Поддерживается" else "НеПоддерживается"}\n")
-            if (ok) {
-                caps.append(resp.readText()).append("\n")
-            }
-
-            resp = config.client.get("${tenant.tmn}/itspaces/api/1.0/configurations") {
-                tenant.defaultHeaders(this)
-            }
-            ok = resp.status == HttpStatusCode.OK && resp.contentType()!!.match(ContentType.Application.Json)
-            caps.append("/itspaces/api/1.0/configurations : rc=${resp.status.value} API=${if (ok) "Поддерживается" else "НеПоддерживается"}\n")
-            if (resp.contentType()!!.match(ContentType.Application.Json)) {
-                val ks = k3.OPConfigurationKeyValue.getMap(resp.readText())
-                ks.entries.forEach {
-                    caps.append("\t${it.key} = ${it.value}\n")
-                }
-            }
-        }
-        return caps
-    }
-}
-
-class CommunicationChannelsPO(val config: Config, val nick: String) {
-    val po: Config.PI
-
-    init {
-        po = config.pi[nick] ?: error("Милостивый государь мой, не можно отыскать пэошку $nick в конфиге")
-    }
-
-    fun getList(): MutableList<CommunicationChannelID> {
-        val req = Envelope(CommunicationChannelQueryRequest())
-        val xml = xmlSoap.encodeToString(req)
-        lateinit var list: MutableList<CommunicationChannelID>
-        runBlocking {
-            val resp = po.postSOAP(config.client, CommunicationChannelQueryRequest.getUrl(po.host), xml)
-            val ccqr: Envelope<CommunicationChannelQueryResponse> = xmlSoap.decodeFromString(resp.readText())
-            list = ccqr.data.channels
-        }
-        return list
-    }
-
-    fun readChannels(lst: MutableList<CommunicationChannelID>): List<CommunicationChannel> {
-        val req2 = Envelope(
-            CommunicationChannelReadRequest("User", lst)
-        )
-        val xml2 = xmlSoap.encodeToString(req2)
-        lateinit var ccs: List<CommunicationChannel>
-        runBlocking {
-            val resp2 = po.postSOAP(config.client, CommunicationChannelReadRequest.getUrl(po.host), xml2)
-            val text2 = resp2.readText()
-            val ccrr: Envelope<CommunicationChannelReadResponse> = xmlSoap.decodeFromString(text2)
-            ccs = ccrr.data.channels
-        }
-        return ccs
-    }
-}
-
-/**
- * Скачивает пакеты и артефакты из среды разработки, выдаёт отчёт
- */
-class DownloadWorkspace(config: Config, nick: String) {
-    val tenant: Config.Tenant
-    lateinit var workspace: Workspace
-
-    init {
-        tenant = config.tenants[nick] ?: error("Non enim album: $nick")
-    }
-
-    suspend fun exec(client: HttpClient) {
-        tenant.loginIfNeeded(client)
-        this.workspace = Workspace(tenant)
-        this.workspace.retrieve(client)
-    }
-}
-
-class ServiceEndpointsCPI(val config: Config, val nick: String) {
-    val tenant: Config.Tenant
-
-    init {
-        tenant = config.tenants[nick] ?: error("Ну куды, куды по помытому и с грязным тенантом: $nick")
-    }
-
-    fun extract(): List<ServiceEndpoint> {
-        lateinit var lst: List<ServiceEndpoint>
-        runBlocking {
-            tenant.loginIfNeeded(config.client)
-            val resp = tenant.getJson(config.client, ServiceEndpoints.getUrl(tenant.tmn, true))
-            val ss = ServiceEndpoints.parse(resp.readText())
-            require(ss.d.__next == "", { "//TODO - listing" })
-            lst = ss.d.results
-        }
-        return lst
-    }
-}
