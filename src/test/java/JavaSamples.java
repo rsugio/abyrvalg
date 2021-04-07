6b89458718c15ef27f7c4f3d05c6deebfce60548
chumpa <chumpa@yandex.ru>
2021-04-07 05:36:54 +0300

    тест на CheckCapabilitiesCPI

--- src/test/java/JavaSamples.java
+++ src/test/java/JavaSamples.java
@@ -1,9 +1,35 @@
+import io.rsug.abyrvalg.CheckCapabilitiesCPI;
+import io.rsug.abyrvalg.Config;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class JavaSamples {
+    Path tmpDir = Paths.get("tmp");
+    Config config;
+
+    @Before
+    public void config() throws IOException {
+        String text = new String(Files.newInputStream(tmpDir.resolve("abyrvalg.conf")).readAllBytes(), StandardCharsets.UTF_8);
+
+        config = Config.Companion.parseHocon(text);
+        config.createClient();
+        config.init();
+    }
 
     @Test
-    public void a() {
-
+    public void checkCapabilitiesCPI() {
+        for (Config.Tenant ten : config.getTenants().values()) {
+            CheckCapabilitiesCPI check = new CheckCapabilitiesCPI(config, ten);
+            System.out.println("\n" + ten.getNick() + "\n");
+            System.out.println(check.exec() + "\n");
+        }
     }
+
+
 }
